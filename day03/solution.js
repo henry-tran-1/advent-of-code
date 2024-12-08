d2e4cf18b9de167e0ee36553d9684a9c08abaad4
Henry Tran <henrytran089@gmail.com>
2024-12-08 23:31:32 +1300

    complete day03: part2

--- day03/solution.js
+++ day03/solution.js
@@ -10,14 +10,18 @@ const summedInstructions = (input) => {
   // creates an array of 'instrucitons' that compose of the shape 'mul(digit(1-3),(digit(1-3)))'
   const instructions = input.match(/mul\(\d{1,3},\d{1,3}\)/g)
 
-  // extracts the numbers, converts them to numbers, multiply them together, and sums together the multipled values
-  const sum = instructions.reduce((total, currentValue) => {
-    const numbers = currentValue.match(/\d{1,3}/g)
-    if (numbers !== null) {
-      return total + Number(numbers[0]) * Number(numbers[1])
-    } else return total
-  }, 0)
-  return sum
+  if (instructions === null) {
+    return 0
+  } else {
+    // extracts the numbers, converts them to numbers, multiply them together, and sums together the multipled values
+    const sum = instructions.reduce((total, currentValue) => {
+      const numbers = currentValue.match(/\d{1,3}/g)
+      if (numbers !== null) {
+        return total + Number(numbers[0]) * Number(numbers[1])
+      } else return total
+    }, 0)
+    return sum
+  }
 }
 
 console.log('Solution to Part 1:', summedInstructions(input))
@@ -33,18 +37,41 @@ console.log('Solution to Part 1:', summedInstructions(input))
 // tail: in the section after the last don't, look for the segment after do(), and use sum fn
 // sum all 3 parts together
 
-const splitDont = input.split("don't")
+// realised the input was on several different lines.  split by linebreak, then rejoined.
+const joinedInput = input.split('\n').join()
 
+const splitDont = joinedInput.split("don't()")
 // handling head section
 const headSection = splitDont[0]
 const headSum = summedInstructions(headSection)
 
 // handling tail section
-const tailSection = splitDont[splitDont.length - 1].match(/do\(\).*/)
-if (tailSection !== null) {
-  const tailSum = summedInstructions()
-} else tailSum = 0
+const tailSection = splitDont[splitDont.length - 1].match(/do\(\).*/g)
 
-console.log('head sum: ', headSum)
-console.log('tail sum: ', tailSum)
+let tailSum
+if (tailSection === null) {
+  tailSum = 0
+} else if (tailSection.length > 1) {
+  tailSection.sort((a, b) => b.length - a.length)
+  tailSum = summedInstructions(tailSection[0])
+} else tailSum = summedInstructions(tailSection[0])
+
+// handling middle section
+const middleSection = splitDont.slice(1, -1)
+const middleSegments = middleSection.map((section) => {
+  const segments = section.match(/do\(\).*/g)
+  if (segments === null) {
+    return ''
+  } else if (segments.length > 1) {
+    segments.sort((a, b) => b.length - a.length)
+    return segments[0]
+  } else return segments[0]
+})
+let middleSum = 0
+middleSegments.forEach((item) => {
+  middleSum += summedInstructions(item)
+})
+const totalSum = headSum + tailSum + middleSum
+
+console.log('Solution to Part 2:', totalSum)
 // -- Part 2 Solved -- //

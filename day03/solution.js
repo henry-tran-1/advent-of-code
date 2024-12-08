673c2f14a5670fce70e899ffd3b055c5c3f0e84d
Henry Tran <henrytran089@gmail.com>
2024-12-08 15:46:32 +1300

    complete day03: part1

--- day03/solution.js
+++ day03/solution.js
@@ -5,6 +5,15 @@ const demoInput = fs.readFileSync('./demo-input.txt', 'utf-8')
 
 // ============================== Part 1 ============================== //
 
+const instructions = input.match(/mul\(\d{1,3},\d{1,3}\)/g)
+
+const sumInstructions = instructions.reduce((total, currentValue) => {
+  const numbers = currentValue.match(/\d{1,3}/g)
+  return total + Number(numbers[0]) * Number(numbers[1])
+}, 0)
+
+console.log('Solution to Part 1:', sumInstructions)
+
 // -- Part 1 Solved -- //
 
 // ============================== Part 2 ============================== //

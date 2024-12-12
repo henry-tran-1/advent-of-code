9127b3bbca64fd1b371e851284b91feb2dd283fb
Henry Tran <henrytran089@gmail.com>
2024-12-12 18:52:22 +1300

    parse input for day06:part1

--- day06/solution.ts
+++ day06/solution.ts
@@ -5,6 +5,9 @@ const demoInput = fs.readFileSync('./demo-input.txt', 'utf-8')
 
 // ============================== Part 1 ============================== //
 
+// parse input into a grid, array of arrays
+const grid: string[][] = demoInput.split('\n').map((line) => line.split(''))
+
 // -- Part 1 Solved -- //
 
 // ============================== Part 2 ============================== //
